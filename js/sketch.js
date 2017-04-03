c2f413795e0fa973e9c4414106faf069c5764b0b
Conner Novicki <connernovicki@gmail.com>
2017-04-03 16:05:35 -0400

    Improved

--- js/sketch.js
+++ js/sketch.js
@@ -1,22 +1,27 @@
 var environment;
-var CANVAS_WIDTH = 600;
-var CANVAS_HEIGHT = 600;
+var CANVAS_WIDTH = 1400;
+var CANVAS_HEIGHT = 800;
 var selection;
 var wind;
 var flowField;
 
-var FLOW_FIELD_RESOLUTION = 100;
+var INITIAL_POPULATION = 50;
+var FLOW_FIELD_RESOLUTION = 20;
 
+var randomTarget;
 var centerVector;
+var debug = false;
+var separation = false;
 
 function setup() {
     createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
     environment = new Environment(CANVAS_WIDTH, CANVAS_HEIGHT);
-    environment.createNewPopulation(50);
+    environment.createNewPopulation(INITIAL_POPULATION);
     environment.wrapEdges();
 
     selection = 0;
     centerVector = createVector(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2);
+    flowField = new FlowField(CANVAS_WIDTH, CANVAS_HEIGHT, FLOW_FIELD_RESOLUTION);
 }
 
 function draw() {
@@ -28,20 +33,51 @@ function draw() {
             break;
         case 1:
             environment.applyForce(wind);
+            if (debug) {
+                environment.drawForce(wind);
+            }
             break;
         case 2:
             var currPos = createVector(mouseX, mouseY);
             var v = currPos.sub(centerVector);
             v.setMag(2);
             environment.applyForce(v);
+            if (debug) {
+                environment.drawForce(v);
+            }
             break;
         case 3:
             environment.applyFlowField(flowField);
+
+            // TODO: Flow field drawing needs debug.
+            /*
+            if (debug) {
+                flowField.display();
+            }
+            */
+            break;
+        case 4:
+            environment.seekTarget(randomTarget);
+            if (debug) {
+                fill(90, 33, 128);
+                ellipse(randomTarget.x, randomTarget.y, 30, 30);
+            }
+            break;
+        case 5:
+            var mousePos = createVector(mouseX, mouseY);
+            environment.blob(mousePos);
+            break;
+        case 6:
+            environment.flock();
             break;
     }
 
+    if (separation) {
+        environment.separate();
+    }
     environment.applyEdges();
     environment.update();
+    resetGraphics();
     environment.display();
 }
 
@@ -49,19 +85,60 @@ function draw() {
 // Trying to get all possibilities for quick switching with buttons.
 
 function randomizeMovement() {
+    environment.edgeType = 1;
     selection = 0;
 }
 
 function randomWind() {
+    environment.edgeType = 1;
     wind = p5.Vector.random2D();
     selection = 1;
 }
 
 function directedWind() {
+    environment.edgeType = 1;
     selection = 2;
 }
 
-function flowField() {
+function getFlowField() {
+    environment.edgeType = 1;
     selection = 3;
-    flowField = new FlowField(CANVAS_WIDTH, CANVAS_HEIGHT, 100);
+    flowField.randomize();
+}
+
+function newTargetPosition() {
+    environment.edgeType = 2;
+    selection = 4;
+    randomTarget = createVector(random(0, width), random(0, height));
+}
+
+function changePopulation(popSize) {
+    environment.changePopulation(popSize);
+}
+
+function seekMouse() {
+    selection = 5;
+}
+
+function toggleVehicleShape() {
+    environment.toggleVehicleShape();
+}
+
+function flockVehicles() {
+    environment.edgeType = 1;
+    selection = 6;
+}
+
+function toggleDebug() {
+    debug = !debug;
+}
+
+function toggleSeparation() {
+    separation = !separation;
+}
+
+function resetGraphics() {
+    strokeWeight(1);
+    stroke(1);
+    fill(0);
 }
--- js/vehicle.js
+++ js/vehicle.js
@@ -4,41 +4,128 @@ var Vehicle = function(posVec, velVec, maxS, maxF) {
     this.acceleration = createVector(0, 0);
 
     this.maxSpeed = maxS;
-    this.maxForce = this.maxSpeed / 10;
+    this.maxForce = maxF;
+    this.radius = 12;
 
-    this.SIZE = 20;
-}
+    this.SIZE = 10;
+
+    this.geometry = 1;
+};
 
 Vehicle.prototype.applyForce = function(force) {
     force.limit(this.maxForce);
     this.acceleration.add(force);
-}
+};
+
+Vehicle.prototype.seek = function(target) {
+    var desired = p5.Vector.sub(target, this.position);
+    desired.limit(this.maxSpeed);
+
+    var steer = p5.Vector.sub(desired, this.velocity);
+    steer.limit(this.maxForce);
+    this.applyForce(steer);
+};
+
+Vehicle.prototype.separation = function(otherVehicles) {
+    var desiredSep = this.radius * 2;
+    var sum = createVector(0, 0);
+    var count = 0;
+
+    for (var i = 0; i < otherVehicles.length; i++) {
+        var v = otherVehicles[i];
+        var d = p5.Vector.dist(this.position, v.position);
+        if ((d > 0) &&(d < desiredSep)) {
+
+            var diff = p5.Vector.sub(this.position, v.position);
+            diff.normalize();
+            diff.div(d);
+            sum.add(diff);
+            count++;
+        }
+    }
+    if (count > 0) {
+        sum.setMag(this.maxSpeed);
+        var steer = p5.Vector.sub(sum, this.velocity);
+        this.applyForce(steer);
+    }
+};
+
+Vehicle.prototype.align = function(otherVehicles) {
+    var desiredSep = this.radius * 2;
+    var sum = createVector(0, 0);
+    var count = 0;
+
+    for (var i = 0; i < otherVehicles.length; i++) {
+        var v = otherVehicles[i];
+        var d = p5.Vector.dist(this.position, v.position);
+        if ((d > 0) &&(d < desiredSep)) {
+
+            sum.add(v.velocity);
+            count++;
+        }
+    }
+    if (count > 0) {
+        sum.div(count);
+        sum.normalize();
+        sum.mult(this.maxSpeed);
+        sum.add(this.velocity);
+        this.applyForce(sum);
+    }
+};
+
+Vehicle.prototype.cohesion = function(otherVehicles) {
+    var desiredSep = this.radius * 2;
+    var sum = createVector(0, 0);
+    var count = 0;
+
+    for (var i = 0; i < otherVehicles.length; i++) {
+        var v = otherVehicles[i];
+        var d = p5.Vector.dist(this.position, v.position);
+        if ((d > 0) &&(d < desiredSep)) {
+
+            var diff = p5.Vector.sub(this.position, v.position);
+            diff.normalize();
+            diff.div(d);
+            sum.add(diff);
+            count++;
+        }
+    }
+    if (count > 0) {
+        sum.div(count);
+        sum.mult(3);
+        this.applyForce(sum);
+    }
+};
 
 Vehicle.prototype.update = function() {
     this.velocity.add(this.acceleration);
-    this.velocity.limit(this.maxSpeed);
+
     this.position.add(this.velocity);
+    this.velocity.limit(this.maxSpeed);
     this.acceleration.mult(0);
-}
+};
 
 Vehicle.prototype.display = function() {
-    var tempVector;
-    if (this.velocity.x == 0 && this.velocity.y == 0) {
-        tempVector = p5.Vector.random2D();
+    if (this.geometry == 1) {
+        var tempVector;
+        if (this.velocity.x === 0 && this.velocity.y === 0) {
+            tempVector = p5.Vector.random2D();
+        } else {
+            tempVector = this.velocity.copy();
+        }
+
+        tempVector.normalize();
+        tempVector.mult(-this.SIZE);
+
+        var rightWing = tempVector.copy();
+        var leftWing = tempVector.copy();
+        rightWing.rotate(PI/8);
+        leftWing.rotate(-PI/8);
+        rightWing.add(this.position);
+        leftWing.add(this.position);
+
+        triangle(this.position.x, this.position.y, rightWing.x, rightWing.y, leftWing.x, leftWing.y);
     } else {
-        tempVector = this.velocity.copy();
+        ellipse(this.position.x, this.position.y, this.SIZE, this.SIZE);
     }
-
-    tempVector.normalize();
-    tempVector.mult(-this.SIZE);
-
-    var rightWing = tempVector.copy();
-    var leftWing = tempVector.copy();
-    rightWing.rotate(PI/8);
-    leftWing.rotate(-PI/8);
-    rightWing.add(this.position);
-    leftWing.add(this.position);
-
-    triangle(this.position.x, this.position.y, rightWing.x, rightWing.y, leftWing.x, leftWing.y);
-
-}
+};
